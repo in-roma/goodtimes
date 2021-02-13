4998d5ac1c38ce258d2684a919ef7cbfad8a6d9d
in-roma <romain.grandjean@outlook.com>
2021-02-13 13:46:57 +0100

    tilt and fade effect on pics

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ desktopHeader.innerHTML = header.innerHTML;
 inView('.header')
 	.on('enter', (el) => desktopHeader.classList.remove('visible'))
 	.on('exit', (el) => desktopHeader.classList.add('visible'));
+
+// Tilt effect on pictures
+
+VanillaTilt.init(document.querySelectorAll('.image'), {
+	max: 35,
+	speed: 800,
+	glare: true,
+	easing: 'cubic-bezier(.03,.98,.52,.99)',
+});
+
+// Fade in pictures
+
+inView('.fade')
+	.on('enter', (img) => img.classList.add('visible'))
+	.on('exit', (img) => img.classList.remove('visible'));
